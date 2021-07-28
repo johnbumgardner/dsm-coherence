ead3c1f0d8717fbcf63d72e8321d06b9dc77240d
John Bumgardner <john.bumgardner@icloud.com>
2021-07-27 21:10:02 -0400

    fix other error

--- ssci.cc
+++ ssci.cc
@@ -65,7 +65,7 @@ void SSCI::sendInt_to_sharer(ulong addr, int num_proc, int proc_num){
 	// Invoke the sendInt function defined in the main function
 	// for all the entries in the list except for proc_num. 
 	for (int p = 0; p < num_proc; p++) {
-		if ((p != proc_num) && bit[p]) {
+		if ((p != proc_num)) {
 			sendInv(addr, p);
 		}
 	}
